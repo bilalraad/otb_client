a0f671b1ae151cd14f10ec069ceb2b4092ff8444
Bilal Raad <bilal52040@gmail.com>
2021-11-01 16:48:28 +0300

    finish setting up firebase and fcm for android

--- android/app/src/main/kotlin/com/otb/otbclient/MainActivity.kt
+++ android/app/src/main/kotlin/com/otb/otbclient/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.otb_client
+package com.otb.otbclient
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'app.dart';
+import './app.dart';
 import 'src/data/API/search_query_service.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   Bloc.observer = AppObserver();
   final _tripsQueryService = MockTripsQueryService();
   runApp(OTBApp(tripsQueryService: _tripsQueryService));
